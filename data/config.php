528dc25ecd3b27e4221f22ca9dc80564c3f49d9f
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2010-01-02 00:52:03 +0530

    Added example packages and some main code in config.php

--- data/config.php
+++ data/config.php
@@ -6,7 +6,17 @@
  * Licence: You may redistribute this under Creative Commons License
  */
 
- $ln = "\n";
- $cwd = getcwd();
+ //define(MODE,"cli" or "web"); This should be in the initial including file in app
+ define(CWD,getcwd());
+
+ defined(MODE) or die('You should define a mode before starting the usage of PDQL');
+
+ if(MODE == "cli")
+	define(N,"\n");
+ else
+	define(N,"<br>");
+
+ if(substr(sprintf('%o', fileperms(CWD)), -4) != "0757")
+	die("File permissions not set correctly. Check the INSTALL file");
 
 ?>
